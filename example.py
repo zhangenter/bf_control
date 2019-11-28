35377751d75aa079f013cfd8d15656fe52fde020
zhangenter <83463386@qq.com>
2019-11-28 23:31:37 +0800

    edit

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 # -*- coding=utf-8 -*-
 import pygame
-from bf_button import BFButton,BFButtonGroup
+from bf_edit import BFEdit
+from bf_button import BFButton
+from bf_panel import BFPanel
 
 pygame.init()
 screencaption = pygame.display.set_caption('bf control')
@@ -17,19 +19,24 @@ def do_click3(btn):
     pygame.quit()
     exit()
 
-btn_group = BFButtonGroup()
-btn_group.make_button(screen, (120,100,160,40),text='Play',click=do_click1)
-btn_group.make_button(screen, (120,180,160,40),text='Hide',click=do_click2)
-btn_group.make_button(screen, (120,260,160,40),text='Quit',click=do_click3)
+btn_panel = BFPanel()
+btn_panel.add_control(BFButton(screen, (120,20,160,40),text='Play',click=do_click1))
+btn_panel.add_control(BFButton(screen, (120,70,160,40),text='Hide',click=do_click2))
+btn_panel.add_control(BFButton(screen, (120,120,160,40),text='Quit',click=do_click3))
+edit1 = BFEdit(screen, (120,170,160,40),text='test1')
+btn_panel.add_control(edit1)
+edit2 = BFEdit(screen, (120,220,160,40),text='test2')
+btn_panel.add_control(edit2)
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
              pygame.quit()
              exit()
-        btn_group.update(event)
+        btn_panel.update(event)
 
     screen.fill((255,255,255))
-    btn_group.draw()
+    btn_panel.draw()
    
     pygame.display.update() 
